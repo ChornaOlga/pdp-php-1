137ea12388ff25e629ce741f8384a9ba7375546d
Alexander Litvinenko <litvinenko1706@gmail.com>
2015-02-02 19:51:20 +0200

    + тестовый обмен инфой с РНР-файлом

--- html/solve.php
+++ html/solve.php
@@ -20,13 +20,11 @@ function get_pdp_points_data_from_json($json)
 }
 
 $response = [];
-file_put_contents('123', var_export($_REQUEST['data'], true));
-if (!empty($_REQUEST) && isset($_REQUEST['data']) && ($params = json_decode($_REQUEST['data'])) && ($params instanceof stdClass))
+if (!empty($_REQUEST) && isset($_REQUEST['params']) && ($params = json_decode($_REQUEST['params'])) && ($params instanceof stdClass))
 {
     $data   = $params->data;
     $method = $params->method;
 
-file_put_contents('123', $params);
     if (in_array($method, $avaliableSolutionMethods))
     {
         $response['path'] = [1,2,3];
@@ -41,6 +39,7 @@ else
 {
     $response['errors'][] = 'Input JSON is broken or absent';
 }
+// file_put_contents('123', var_export($_REQUEST, true));
 file_put_contents('123', var_export($params, true));
 
 echo json_encode($response);
