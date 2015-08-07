be699a337b544f885cf9c5ed3a083927979c1847
Alexander Litvinenko <litvinenko1706@gmail.com>
2015-08-07 19:07:19 +0300

    + db dump

--- demo/test_all.php
+++ demo/test_all.php
@@ -3,7 +3,7 @@ require_once '../vendor/autoload.php';
 include 'PdpLauncher.php';
 
 $generateRandomData      = true;
-$dummyMode               = true;
+$dummyMode               = false;
 
 $dummyOutputCsvFile      = 'dummy.csv';
 $productionOutputCsvFile = 'result.csv';
@@ -12,8 +12,8 @@ $outputCsvFile           = $dummyMode ? $dummyOutputCsvFile : $productionOutputC
 $pairCountToTest         = [/*3,4,*/5,6,7,8];
 $repeatEachTestCount     = 5;
 
-$pairCountToTest         = [2/*5,6,7*/];
-$repeatEachTestCount     = 5;
+// $pairCountToTest         = [35,6,7];
+// $repeatEachTestCount     = 5;
 
 $genPrecises = [
 // pair count => all precices to try
@@ -26,9 +26,9 @@ $genPrecises = [
 ];
 
 $allLoadParams = [
-//   // ['weight_capacity' => 100,  'load_area' => ['x' => 50, 'y' => 50, 'z' => 50]],
-//   // ['weight_capacity' => 150,  'load_area' => ['x' => 50, 'y' => 50, 'z' => 50]],
-  // ['weight_capacity' => 200, 'load_area' => ['x' => 50, 'y' => 50, 'z' => 50]],
+  ['weight_capacity' => 100,  'load_area' => ['x' => 50, 'y' => 50, 'z' => 50]],
+  ['weight_capacity' => 150,  'load_area' => ['x' => 50, 'y' => 50, 'z' => 50]],
+  ['weight_capacity' => 200, 'load_area' => ['x' => 50, 'y' => 50, 'z' => 50]],
   ['weight_capacity' => 300, 'load_area' => ['x' => 50, 'y' => 50, 'z' => 50]],
   // ['weight_capacity' => 400, 'load_area' => ['x' => 50, 'y' => 50, 'z' => 50]],
   // ['weight_capacity' => 500, 'load_area' => ['x' => 50, 'y' => 50, 'z' => 50]],
@@ -71,7 +71,7 @@ $allLoadParams = [
 ];
 
 
-try {$dbh = new PDO('mysql:host=localhost;dbname=pdp', 'root', '');} catch (PDOException $e) {print "Error!: " . $e->getMessage() . "<br/>"; die(); }
+try {$dbh = new PDO('mysql:host=localhost;dbname=pdp', 'root', 'adMiN8910');} catch (PDOException $e) {print "Error!: " . $e->getMessage() . "<br/>"; die(); }
 
 $launcher = new PdpLauncher;
 $params = str_replace("'","",var_export(compact('generateRandomData', 'dummyMode', 'pairCountToTest', 'repeatEachTestCount', 'genPrecises', 'allLoadParams'), true));
@@ -125,8 +125,6 @@ foreach ($pairCountToTest as $pairCount)
       $prefix  = $exact_solution_info;
       $postfix = ",\"" . $dataPrepared . "\",\"" . $pdpPointsPrepared . "\"";
 
-      if (!$dbh->query("INSERT INTO results(test_id,time,cost,cost_increase,path) VALUES ((select max(id) from tests),{$exactSolution['solution_time']},{$exactSolution['path_cost']},0,'".implode(' ',$exactSolution['path'])."')")) print_r($dbh->errorInfo());
-
       foreach($genPrecises[$pairCount] as $precise)
       {
         $genSolution = $launcher->getSolution('gen', $data, [ 'precise' => $precise, 'weight_capacity' => $loadParams['weight_capacity'], 'load_area' => $loadParams['load_area'] ]);
@@ -139,11 +137,73 @@ foreach ($pairCountToTest as $pairCount)
 
         file_put_contents($outputCsvFile, $newLine, FILE_APPEND);
 
-        if (!$dbh->query("INSERT INTO results(test_id,time,cost,cost_increase,path) VALUES ((select max(id) from tests),{$genSolution['solution_time']},{$genSolution['path_cost']},$costIncrease,'".implode(' ',$genSolution['path'])."')")) print_r($dbh->errorInfo());
+        if (!$dbh->query("INSERT INTO results(test_id,time,precise,cost,cost_increase,path) VALUES ((select max(id) from tests),{$genSolution['solution_time']},$precise,{$genSolution['path_cost']},$costIncrease,'".implode(' ',$genSolution['path'])."')")) print_r($dbh->errorInfo());
       }
+
+      if (!$dbh->query("INSERT INTO results(test_id,time,precise,cost,cost_increase,path) VALUES ((select max(id) from tests),{$exactSolution['solution_time']},100,{$exactSolution['path_cost']},0,'".implode(' ',$exactSolution['path'])."')")) print_r($dbh->errorInfo());
     }
   }
 }
 
 if (!$dbh->query("set @id = (select max(id) from test_suites); UPDATE test_suites SET end_time=NOW() WHERE id=@id")) print_r($dbh->errorInfo());
 $dbh = null;
+
+/* SQL to view results:
+select s.id as suite_id,s.start_time,s.end_time,t.id as test_id,t.pair_count,t.load_area_size,t.weight_capacity,r.time,r.cost,r.precise,r.cost_increase,t.data,t.pdp_points_txt from test_suites s, tests t, results r
+where s.id=t.test_suite_id and t.id=r.test_id
+order by s.id,t.pair_count,t.load_area_size,t.weight_capacity
+*/
+
+/* db dump:
+-- --------------------------------------------------------
+--
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+// CREATE DATABASE IF NOT EXISTS `pdp` /*!40100 DEFAULT CHARACTER SET latin1 */;
+// USE `pdp`;
+
+
+// CREATE TABLE IF NOT EXISTS `results` (
+//   `id` int(11) NOT NULL AUTO_INCREMENT,
+//   `test_id` int(11) DEFAULT NULL,
+//   `time` float NOT NULL DEFAULT '0',
+//   `cost` float NOT NULL DEFAULT '0',
+//   `precise` tinyint(4) NOT NULL DEFAULT '0',
+//   `cost_increase` float NOT NULL DEFAULT '0',
+//   `path` varchar(50) NOT NULL DEFAULT '0',
+//   PRIMARY KEY (`id`),
+//   KEY `FK__tests` (`test_id`),
+//   CONSTRAINT `FK__tests` FOREIGN KEY (`test_id`) REFERENCES `tests` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+// ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+// CREATE TABLE IF NOT EXISTS `tests` (
+//   `id` int(11) NOT NULL AUTO_INCREMENT,
+//   `test_suite_id` int(11) NOT NULL,
+//   `pair_count` int(11) NOT NULL,
+//   `load_area_size` int(11) NOT NULL,
+//   `weight_capacity` int(11) NOT NULL,
+//   `data` text,
+//   `pdp_points_txt` text,
+//   PRIMARY KEY (`id`),
+//   KEY `FK_tests_test_suites` (`test_suite_id`),
+//   CONSTRAINT `FK_tests_test_suites` FOREIGN KEY (`test_suite_id`) REFERENCES `test_suites` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+// ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+// CREATE TABLE IF NOT EXISTS `test_suites` (
+//   `id` int(11) NOT NULL AUTO_INCREMENT,
+//   `start_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+//   `end_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+//   `params` mediumtext NOT NULL,
+//   PRIMARY KEY (`id`)
+// ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+// /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+// /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+// /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
